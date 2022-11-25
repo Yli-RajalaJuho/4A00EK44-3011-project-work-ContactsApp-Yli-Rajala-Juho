c3f32f15c384880fb1929329476ff9fc75861742
Yli-RajalaJuho <juho.yli-rajala@tuni.fi>
2022-11-25 16:15:22 +0200

    groudwork for project

---
+++ src/ContactsApp.java
@@ -0,0 +1,11 @@
+
+/**
+* The class ContactsApp is used to run the final application.
+*
+* @author Juho Yli-Rajala
+*/
+public class ContactsApp {
+    public static void main(String[] args) {
+        //Main Application
+    }
+}
